c4f857877c281fdf8e32f87d11557bfa221c5350
marypodst <marypodst@mail.ru>
2024-04-14 13:11:11 +0300

    Мой первый коммит!

---
+++ Seleniumtests_Podstavlennikova/SeleniumTestsForPractic.cs
@@ -0,0 +1,48 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Seleniumtests_Podstavlennikova;
+
+public class SeleniumTestsForPractic
+{
+    [Test]
+    public void Authorization()
+    {
+        var options = new ChromeOptions();
+        options.AddArguments("--no-sandbox", "--start-maximized", "--disable-extensions");
+        
+        //зайти в хром с помощью веб драйвера
+        var driver = new ChromeDriver();
+        
+        //перейти по урлу https://staff-testing.testkontur.ru/ 
+        driver.Navigate().GoToUrl("https://staff-testing.testkontur.ru");
+        Thread.Sleep(3000);
+        
+        //ввести логин и пароль
+        var login = driver.FindElement(By.Id("Username"));
+        login.SendKeys("marypodst@mail.ru");
+
+        var password = driver.FindElement(By.Name("Password"));
+        password.SendKeys("YaMahaATiNet2.");
+        Thread.Sleep(3000);
+        
+        //нажать кнопку Войти       
+        var enter = driver.FindElement(By.Name("button"));
+        enter.Click();
+        Thread.Sleep(3000);
+
+        
+        //проверить, что находимся на нужной странице
+        var currentUrl = driver.Url;
+        Assert.That(currentUrl == "https://staff-testing.testkontur.ru/news");
+        
+      
+       
+        
+        //закрыть браузер и убить процесс драйвера
+        driver.Quit();
+    }
+    
+    
+}
